cb66b15ea321275e95726961585e0368178f0f75
csw1511 <csw3411@gmail.com>
2022-11-21 08:40:05 +0000

    refact : 쓸모없는 파일과 코드 삭제 - component 내의 MAIN.js APP.js Login.js, server.js의 필요없는 테스트 코드

--- server/server.js
+++ server/server.js
@@ -7,26 +7,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(cors());
 
-let id = 2;
-const todoList = [{
-    id: 1,
-    text: '할일 1',
-    done: false,
-}];
-
-app.get('/api/todo', (req, res) => {
-    res.json(todoList);
-})
-
-app.post('/api/todo', (req, res) => {
-    const { text, done } = req.body;
-    todoList.push({
-        id: id++,
-        text,
-        done,
-    });
-    return res.send('success');
-});
 
 
 app.use('/user_inform', user_inform);
